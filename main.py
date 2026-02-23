ea411e0b50fef1f5700273c0a8b06a5bce214857
aayush-aryan-0 <aayushar1601l@gmail.com>
2026-02-24 01:14:01 +0530

    trying to add html

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from task import Task
+from database import *
+
 app=Flask(__name__)
+
+
 @app.route('/')
-def hello():
-    return '<h2>Welcome to my todo list app</h2>'
+def home():
+    return render_template('index.html',tasks=ToDoDB.readToDoDB())
+
+@app.route("/add",methods=['POST'])
+def add():
+    try:
+        ToDoDB.addTask("",0)
+    except Exception as e:
+        print(e)
+    return redirect('/')
+
+@app.route("/save",methods=['POST'])
+def save():
+    try:
+        text=text = request.form.get("text")
+        done=text = request.form.get("done")
+
+        if(done):
+            ToDoDB.addTask(text,1)
+        else:
+            ToDoDB.addTask(text,0)
+    except Exception as e:
+        print(e)
+    return redirect('/') 
+    
+@app.route("/delete",methods=['POST'])
+def delete():
+    try:
+        text=text = request.form.get("text")
+        ToDoDB.deleteTask(text)
+    except Exception as e:
+        print(e)
+    return redirect('/') 
+
+
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=50001,debug=True)
--- task.py
+++ task.py
@@ -1,15 +1,19 @@
 class Task:
-    __task=""
+    __text=""
     __done=False
-    def __init__(self,task,done):
-        self.__task=task
+    def __init__(self,text,done):
+        self.__text=text
         self.__done=done
     @property
-    def task(self):
-        return self.__task
+    def text(self):
+        return self.__text
     @property
     def done(self):
         return self.__done
     @done.setter
     def done(self,done):
         self.__done=done
+    @done.setter
+    def text(self,text):
+        self.__text=text
+    
